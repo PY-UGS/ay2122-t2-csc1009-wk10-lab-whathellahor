60ad6bbbf0283efe9dabb4f1bf8db723d361b520
Ng Zi Hao <zihaoe1998@gmail.com>
2022-03-28 14:59:50 +0800

    Update Question3.py

--- Question3.py
+++ Question3.py
@@ -1,22 +1,22 @@
 # Function to return the module name based on the module code
 def getModule(code):
-    match code:
-        case "CSC1006":
+    match code:  # Similar to switch case in java
+        case "CSC1006":  # If the code passed in is CSC1006, returns the following string
             return "Mathematics II"
-        case "CSC1007":
+        case "CSC1007":  # If the code passed in is CSC1007, returns the following string
             return "Operating Systems"
-        case "CSC1008":
+        case "CSC1008":  # If the code passed in is CSC1008, returns the following string
             return "Data Structure and Algorithms"
-        case "CSC1009":
+        case "CSC1009":  # If the code passed in is CSC1009, returns the following string
             return "Object-Oriented Programming"
-        case "CSC1010":
+        case "CSC1010":  # If the code passed in is CSC1010, returns the following string
             return "Computer Networks"
 
 
 # Function to print out all odd numbers from numbers 102 - 66
 def printOddNum():
-    for i in range(102, 66, -1):
-        if i % 2 != 0:
+    for i in range(102, 66, -1):  # For loop to run through the numbers in descending order
+        if i % 2 != 0:  # Check only odd numbers and print it
             print(i)
 
 
